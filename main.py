2d5272e0c2293c825f2cf1d272f8250a4f42c5c5
John-Trunix <john@trunix.ch>
2021-11-04 22:52:46 +0100

    working upload

--- main.py
+++ main.py
@@ -32,13 +32,16 @@ def get_certificate_url_from_mail():
         status, response = imap.fetch(email_id, "(RFC822)")
         raw_email = response[0][1]
         raw_email_string = raw_email.decode("utf-8")
-        email_message = email.message_from_string(
-            raw_email_string).get_payload(decode=True)
+        email_message = email.message_from_string(raw_email_string).get_payload(
+            decode=True
+        )
 
         if "https://my.easytesting.ch/api/cert/" in str(email_message):
 
             match = re.search(
-                'https?:\/\/(my.easytesting.ch\/api\/cert\/)([-a-zA-Z0-9]*)', str(email_message))
+                "https?:\/\/(my.easytesting.ch\/api\/cert\/)([-a-zA-Z0-9]*)",
+                str(email_message),
+            )
 
             if match == None:
                 return print("No URL in Message found!")
@@ -51,7 +54,7 @@ def get_certificate_url_from_mail():
 
 def download_pdf(url):
     r = requests.get(url, allow_redirects=True)
-    open('certificate.pdf', 'wb').write(r.content)
+    open("certificate.pdf", "wb").write(r.content)
 
 
 def extract_date_from_pdf(file_path):
@@ -59,15 +62,50 @@ def extract_date_from_pdf(file_path):
         pdf_content = pdf.pages[0].extract_text()
 
         match = re.search(
-            '([0-9A-Z]* Datum und Zeit der )(.[.,: 0-9]+)', pdf_content)
+            "([0-9A-Z]* Datum und Zeit der )(.[.0-9]+)", pdf_content)
 
         timestamp = match.group(2)
         print(timestamp)
+        return timestamp
+
+
+def intranet_login(username, password):
+    headers = {"content-type": "application/x-www-form-urlencoded"}
+    data = "user=" + username + "&pass=" + password + "&logintype=login"
+
+    r = requests.post(
+        "https://my.fhgr.ch/index.php?id=home", headers=headers, data=data
+    )
+
+    match = re.search("(fe_typo_user=[0-9a-z]*)", r.headers["set-cookie"])
+    session_token = match.group(1)
+
+    print("Got Session Token: " + session_token)
+    return session_token
+
+
+def upload_pdf(session_token, typ, date, result, file_path):
+    headers = {"Cookie": session_token}
+    data = {
+        'tx_htwnotadr_user[covidzert][typ]': typ,
+        'tx_htwnotadr_user[covidzert][date]': date,
+        'tx_htwnotadr_user[covidzert][result]': result,
+        'file_nachweis': open(file_path, 'rb')
+    }
+
+    r = requests.post(
+        "https://my.fhgr.ch/index.php?id=home", headers=headers, data=data
+    )
+
+    print(r.status_code)
 
 
 url = get_certificate_url_from_mail()
 download_pdf(url)
-extract_date_from_pdf('./certificate.pdf')
+timestamp = extract_date_from_pdf("./certificate.pdf")
+session_token = intranet_login(INTRANET_USERNAME, INTRANET_PASSWORD)
+upload_pdf(session_token, "2", timestamp, "1", 'certificate.pdf')
+
 
 # In Funktion einbauen
 status = imap.close()
